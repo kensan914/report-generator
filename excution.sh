01ad3a9384762c4ea44b007645dc245be5b1b095
鳥海健人 <toriumi@ywlan140230.sras.sic.shibaura-it.ac.jp>
2019-07-01 15:50:40 +0900

    shell script

--- excution.sh
+++ excution.sh
@@ -4,4 +4,9 @@ cd `dirname $0`
 script -q output.log ./a.out $@
 rm a.out
 rm cla.sh
-ps aux | grep Terminal | grep -v grep | awk '{ print "kill -9", $2 }' | sh
+PIDarr=(`ps aux | grep Terminal | grep -v grep | awk '{ print $2 }'`)
+echo ${PIDarr[@]} | awk '{
+    max = $1
+    for(i = 2; i <= NF; i++) max = $i > max ? $i : max;
+    print "kill -9", max
+}' |sh
